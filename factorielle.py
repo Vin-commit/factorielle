4181d64ea3f36d89af6cb20d7873fe00d30eb947
Vin-commit <74498336+Vin-commit@users.noreply.github.com>
2020-11-16 18:55:08 +0100

    Add files via upload

---
+++ factorielle.py
@@ -0,0 +1,40 @@
+﻿#!/usr/bin/python3
+# coding: utf-8
+
+
+# Il y a plusieurs façons de calculer la factorielle sans faire d’appel récursif pour un même résultat.
+
+
+def factorielle(n):
+    i = n
+    calcul_correspondant = str(n)+”!=”
+    while (i > 1):
+       n = n *  (i - 1)
+       calcul_correspondant = calcul_correspondant + str(i) +”*”
+       i = i - 1
+     print (calcul_correspondant + “1”)
+     return n
+     
+try:
+  n = int(input("Entrez un nombre dont on veut la factorielle : "))
+  print (“Sa factorielle est”, factorielle(n))
+except:
+  print (“Veuillez saisir un nombre.”)
+----------------------------------------------------------------------- Résultat ---------------------------------------------------------------------------
+  
+
+~                                                                        
+~                                                                                  
+~                                                                                   
+~                                                                                   
+~                                                                                   
+~                                                                                   
+~                                                                                   
+~                                                                                   
+~                                                                                   
+~                                                                                   
+~                                                                                   
+~                                                                                   
+~                                                                                   
+~                                                                                   
+"factorielle" 9 lines, 210 characters
